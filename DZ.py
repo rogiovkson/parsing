7aed4b5c4932af7ab811516d4cf0eed991471475
rogiovkson <rogivokson@gmail.com>
2021-07-28 18:12:30 +0300

    lesson_1

---
+++ DZ.py
@@ -0,0 +1,15 @@
+
+import requests
+import os
+import pprint
+import json
+url = 'https://api.github.com'
+user='rogiovkson'
+r = requests.get(f'{url}/users/{user}/repos')
+
+with open('data.json', 'w') as f:
+    json.dump(r.json(), f)
+
+
+for i in r.json():
+    print(i['name'])
